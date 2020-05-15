7150a8c9769c0a2008ae7b442f98cd4be159e6e6
Cameron Winney <coolcam1111@gmail.com>
2020-05-15 18:53:17 -0500

    Fin

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,37 @@
-let diceRolled = 0
-let diceResult = []
-let numDice = document.getElementById('numDice')
+let diceArray = []
 
-function getRolling() {
-    console.log(numDice)
+function reset() {
+    diceArray = []
+    document.getElementById("rollDisplay").innerHTML = ""
+    document.getElementById("dieContainer").innerHTML = ""
 }
 
+function rollDice() {
+    let numDice = document.getElementById("diceNum").value;
+    var container = document.getElementById("dieContainer");
+        container.innerHTML = ""
+        diceArray = []
+    for (var i = 0; i < numDice; i++) {
+        var diceRoll = Math.floor(Math.random() * 6) + 1;
+        diceArray.push(diceRoll)
+        console.log(diceArray)
+      }
+      let position2 = 0
+      let sum = 0
+        while(position2 < diceArray.length) {
+            sum += diceArray[position2]
+            position2 += 1
+            console.log(sum)
+        }
+        document.getElementById("rollDisplay").innerHTML = "your total was " + sum
+
+      diceNum.value = "";
+    }
+
+    function dispRoll() {
+        let position = 0
+        while(position < diceArray.length) {
+            document.getElementById("dieContainer").innerHTML += '<li>' + diceArray[position] + '</li>'
+            position += 1
+        }
+    }
